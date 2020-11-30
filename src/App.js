df8bb4f8455376d9f4aacea8d30bf5a21fba71a7
DGorgula <gil@dankner.org>
2020-11-30 22:47:22 +0200

    footer added

--- src/App.js
+++ src/App.js
@@ -11,6 +11,13 @@ import Partners from './comps/partners';
 import Donation from './comps/donation';
 import Main from './comps/main';
 import { SiFacebook, SiInstagram } from 'react-icons/si';
+<<<<<<< HEAD
+
+
+
+
+=======
+>>>>>>> footer added
 
 function App() {
 // onMouseLeave={() => setIsShown(false)}  
@@ -144,6 +151,7 @@ function App() {
 
       </Switch>
     </div>
+<<<<<<< HEAD
    
       <footer class="page-footer font-small blue bg-dark">
        <div class="row">
@@ -162,6 +170,24 @@ function App() {
           </div>
       </footer>
 
+=======
+     <footer class="page-footer font-small blue bg-dark">
+  <div class="row">
+    <div class="footer-menu text-center col-12 text-right text-white">
+      <Link to="/" class="p-4 text-white"> אודות</Link> |
+      <Link to="/Information" class="p-4 text-white"> מידע שימושי</Link> | 
+      <Link to="/Donation" class="p-4 text-white"> תרמו לנו</Link> |
+      <Link to="/Partners" class="p-4 text-white"> שותפים</Link>   |
+      <Link to="/Contact" class="p-4 text-white"> צור קשר</Link> 
+     </div>
+ </div>
+
+ <div class="footer-icons text-dark text-center py-3">
+       <SiInstagram size="1.5em" color="white" />--   
+       <SiFacebook size="1.5em" color="white" px-3 />
+     </div>
+ </footer>
+>>>>>>> footer added
     </Router>
 
     
