4108875dd90c06b6bd566a54b48afba33b8dbb61
DGorgula <gil@dankner.org>
2020-11-30 22:53:14 +0200

    footer added, closure before rebase to master

--- src/App.js
+++ src/App.js
@@ -37,51 +37,10 @@ function App() {
       <li className="nav-item my-auto">
         <Link className="nav-link" onMouseEnter={() => setIsShown(true)} to="#">קהילות</Link>
 =======
-    <div className="App">   
-      <header className="container-fluid bg-dark text-white text-right" style={{height:75}}>
-        <div class="row">
-          <div class="logo col-2 text-right">
-           <Link to="/" class="p-0 text-white"> התחבר</Link> |
-           <Link to="/Register" class="p-0 text-white"> הרשם</Link> | 
-           <Link to="/Contact" class="p-0 text-white"> צור קשר</Link>   
-          </div>
-          <div class="menu1 col-9">
-          <Link to="/Main" class="p-4 text-white"> דף הבית</Link> |
-           <Link to="/Community" class="p-4 text-white"> קהילות</Link> |
-           <Link to="/Information" class="p-4 text-white"> מידע שימושי</Link> | 
-           <Link to="/Partners" class="p-4 text-white"> שותפים</Link> |
-           <Link to="/Donation" class="p-4 text-white"> תרמו לנו</Link> 
-          </div>
-          <div class="menu2 col-1"> גמני </div>
-        </div>
-        
-        <div class="row">
-    
-        <div class="col-4"></div>
-        <div class="col-4 input-group py-1 border-dark">
-        <input type="text" class="form-control border-white text-right" placeholder="חפש לי מחלה\קרבה" />
-        <select class="bg-white" style={{border:0}}>
-             <option value="a"></option>
-             <option value="b">b</option>
-             <option value="c">c</option>
-             <option value="d">d</option>
-
-        </select>
-        </div>
-        <div class="col-4"></div>
-        
-        </div>
-      </header>
-    <div className="App">
-    <div onMouseLeave={() => setIsShown(false)}> 
-    <nav className="navbar navbar-expand-lg navbar-dark text-white bg-dark">
-  <Link className="navbar-brand text-center" onMouseEnter={() => setIsShown(false)} to="/main">גמני<br/>אנחנו לא לבד</Link>
-
-  <div className="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul className="navbar-nav ml-auto">
-      <li className="nav-item my-auto">
-        <Link className="nav-link" onMouseEnter={() => setIsShown(true)} to="/communities">קהילות</Link>
+<<<<<<< HEAD
 >>>>>>> rtl site + new header + communities special menu
+=======
+>>>>>>> footer added, closure before rebase to master
       </li>
       <li className="nav-item my-auto" onMouseEnter={() => setIsShown(false)}>
         <Link className="nav-link" to="/Information">מידע שימושי</Link>
@@ -171,22 +130,6 @@ function App() {
       </footer>
 
 =======
-     <footer class="page-footer font-small blue bg-dark">
-  <div class="row">
-    <div class="footer-menu text-center col-12 text-right text-white">
-      <Link to="/" class="p-4 text-white"> אודות</Link> |
-      <Link to="/Information" class="p-4 text-white"> מידע שימושי</Link> | 
-      <Link to="/Donation" class="p-4 text-white"> תרמו לנו</Link> |
-      <Link to="/Partners" class="p-4 text-white"> שותפים</Link>   |
-      <Link to="/Contact" class="p-4 text-white"> צור קשר</Link> 
-     </div>
- </div>
-
- <div class="footer-icons text-dark text-center py-3">
-       <SiInstagram size="1.5em" color="white" />--   
-       <SiFacebook size="1.5em" color="white" px-3 />
-     </div>
- </footer>
 >>>>>>> footer added
     </Router>
 
