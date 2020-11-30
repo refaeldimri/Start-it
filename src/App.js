fec34c31c0562cca98531296ca5c0b8d15749d71
refaeldimri <rafaeldimri@gmail.com>
2020-11-30 16:48:32 +0200

    new footer

--- src/App.js
+++ src/App.js
@@ -7,16 +7,13 @@ import Information from './comps/information';
 import Partners from './comps/partners';
 import Donation from './comps/donation';
 import Main from './comps/main';
-
-
-
-
+import { SiFacebook, SiInstagram } from 'react-icons/si';
 
 function App() {
   return ( 
   <Router>
     <div className="App">   
-      <header className="container-fluid bg-dark text-white text-right " style={{height:75}}>
+      <header className="container-fluid bg-dark text-white text-right" style={{height:75}}>
         <div class="row">
           <div class="logo col-2 text-right">
            <Link to="/" class="p-0 text-white"> התחבר</Link> |
@@ -50,7 +47,6 @@ function App() {
         
         </div>
       </header>
-      
       <Switch>
         <Route exact path="/" component = {Login} />
         <Route exact path="/register" component = {Register} />
@@ -61,12 +57,29 @@ function App() {
         <Route exact path="/donation" component = {Donation} />
         <Route exact path="/main" component = {Main} />  
 
-        
-
-        
       </Switch>
     </div>
+   
+      <footer class="page-footer font-small blue bg-dark">
+       <div class="row">
+         <div class="footer-menu text-center col-12 text-right text-white">
+           <Link to="/" class="p-4 text-white"> אודות</Link> |
+           <Link to="/Information" class="p-4 text-white"> מידע שימושי</Link> | 
+           <Link to="/Donation" class="p-4 text-white"> תרמו לנו</Link> |
+           <Link to="/Partners" class="p-4 text-white"> שותפים</Link>   |
+           <Link to="/Contact" class="p-4 text-white"> צור קשר</Link> 
+          </div>
+      </div>
+
+      <div class="footer-icons text-dark text-center py-3">
+            <SiInstagram size="1.5em" color="white" />--   
+            <SiFacebook size="1.5em" color="white" px-3 />
+          </div>
+      </footer>
+
     </Router>
+
+    
   );
 }
 
