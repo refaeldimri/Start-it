e312cf33878601b14319bd9fe2ea674f2b482d5d
DGorgula <gil@dankner.org>
2020-12-06 15:44:48 +0200

    updated by last master, footer and header added, some style fixes

--- src/App.js
+++ src/App.js
@@ -97,16 +97,13 @@ function App() {
         <Route exact path="/main" component = {Main} />  
 
       </Switch>
-    </div>
       <footer class="page-footer font-small blue bg-dark">
-       <div class="row">
-         <div class="footer-menu text-center col-12 text-right text-white">
+         <div class="footer-menu text-center max-width text-right text-white">
            <Link to="/" class="p-4 text-white"> אודות</Link> |
            <Link to="/Information" class="p-4 text-white"> מידע שימושי</Link> | 
            <Link to="/Donation" class="p-4 text-white"> תרמו לנו</Link> |
            <Link to="/Partners" class="p-4 text-white"> שותפים</Link>   |
            <Link to="/Contact" class="p-4 text-white"> צור קשר</Link> 
-          </div>
       </div>
 
       <div class="footer-icons text-dark text-center py-3">
@@ -114,6 +111,7 @@ function App() {
             <SiFacebook size="1.5em" color="white" px-3 />
           </div>
       </footer>
+    </div>
     </Router>
 
     
